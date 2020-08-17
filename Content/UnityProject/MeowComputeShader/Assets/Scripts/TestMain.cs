bc4bbfdbd241597a37e93cfcb8be1909d1722b1c
Meow <443044187@qq.com>
2020-08-17 20:36:37 +0800

    ComputeShader Test

---
+++ Content/UnityProject/MeowComputeShader/Assets/Scripts/TestMain.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public struct VecMatPair
+{
+    public Vector3 point;
+    public Matrix4x4 matrix;
+}
+
+public class TestMain : MonoBehaviour
+{
+    public ComputeShader shader;
+    private void Start()
+    {
+        RunShader();
+    }
+    private void RunShader()
+    {
+        int kernelHandle = shader.FindKernel("CSMain");
+        RenderTexture tex = new RenderTexture(256, 256, 24);
+        tex.enableRandomWrite = true;
+        tex.Create();
+
+        shader.SetTexture(kernelHandle, "Result", tex);
+        shader.Dispatch(kernelHandle, 256 / 8, 256 / 8, 1);
+    }
+}
